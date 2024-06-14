2521f1aefa4203a82376f68f8fb3e365bd7abcff
Student <you@example.com>
2024-06-14 13:51:27 +0200

    towrzenie windy

---
+++ winda.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "winda.h"
+using namespace std;
+int Winda::pietra = 0;
+int Winda::pietro(){
+    return pietra;
+}
+Winda::Winda(int ml, int mp) {
+    ml = maxludzi;
+    mp = maxpietro;
+}
---
+++ winda.h
@@ -0,0 +1,18 @@
+//
+// Created by Student on 14.06.2024.
+//
+
+#ifndef UNTITLED26_WINDA_H
+#define UNTITLED26_WINDA_H
+class Winda {
+private:
+    int maxludzi;
+    int maxpietro;
+    static int pietra;
+public:
+    static int pietro();
+    Winda(int ml, int mp);
+
+
+};
+#endif //UNTITLED26_WINDA_H
