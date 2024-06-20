93c3c4da726f0ab5bdc3b895fa1fce5480b757ac
JakubPilarski <250991@edu.p.lodz.pl>
2024-06-20 18:30:59 +0200

    lepsza winda

--- main.cpp
+++ main.cpp
@@ -2,33 +2,57 @@
 #include "winda.h"
 using namespace std;
 int main() {
-int ml,mp,l,p,wl;
+int ml,mp,l,p,wl,bl,x;
+
     cout<<"STWORZ SWOJA WINDE"<<endl<< "Na ile pieter moze wjechac?:";
     cin>> mp;
     cout<<endl<< "ile maksimum ludzi moze wsiasc?:";
     cin>> ml;
     cout << endl;
     Winda w(ml, mp);
-        cout<<"Ile osob wsiada?:";
-        cin>> l;
-        cout<<endl<<"Na ktore pietro chca wjechac?:";
-        cin >> p;
-        cout<< endl;
-        w.wsiada(l);
-        w.wybor(p);
-        w.show();
-        while(1){
+    int bp = w.pietro();
+    while(1){
+            while (bp!=0){
+                bl = w.ludziee();
         cout<<"Ile osob wysiada?:";
         cin>>wl;
+        if( wl>bl)
+        {
+            cout<<"Nie ma tylu ludzi w windzie"<<endl;
+        }
+        else
+        {
+            w.wysiada(wl);
+            break;
+        }
+            }while (1){
+                bl = w.ludziee();
         cout<<"Ile osob wsiada?:";
         cin>> l;
+                x = l + bl;
+                if ( x > ml){
+                    cout<< "Nie ma tyle miejsca "<< endl;
+                }
+                else{
+         w.wsiada(l);
+         break;
+                }
+           }
+            while (1){
         cout<<endl<<"Na ktore pietro chca wjechac?:";
         cin>> p;
         cout<< endl;
-        w.wysiada(wl);
-        w.wsiada(l);
-        w.wybor(p);
-        w.show();
+        if(p>mp||p<0)
+        {
+            cout<<"NIE MA TAKIEGO PIETRA"<<endl;
+        } else {
+            w.wybor(p);
+            break;
+        }
+            }
+            w.show();
+
+
 
     }
 }
